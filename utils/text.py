1d8cbc1f62a40704743a5f96cfb7c00b9a74832b
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-05-31 11:37:56 +0100

    implement button text parsing function

--- utils/text.py
+++ utils/text.py
@@ -1,11 +1,22 @@
 import yaml
 
 
+def open_locale_file() -> None:
+    global contents
+
+    text_file = open('locale.yaml')
+    contents = yaml.load(text_file, Loader=yaml.FullLoader)
+
+
 def text(key: str):
     """
     Reads texts from locale.yaml file. Return the string of the given key.
     """
-    
-    text_file = open('locale.yaml')
-    contents = yaml.load(text_file, Loader=yaml.FullLoader)
     return contents['texts'][key]
+
+
+def button(key: str):
+    """
+    Reads texts from locale.yaml file. Return the string of the given key.
+    """
+    return contents['texts']['buttons'][key]
