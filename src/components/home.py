0bb438bc8846d910dc8fef68f60ff98724e7d5fd
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-06-01 12:53:22 +0100

    exchange page from menu

--- src/components/home.py
+++ src/components/home.py
@@ -1,6 +1,6 @@
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove, Update, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext
-from utils.text import text, button
+from utils.text import text, button, custom
 from ..main import logger
 
 
@@ -22,8 +22,36 @@ class Home():
 
         return "HOME_DISPLAY"
 
-    def rate_info(self, update: Update, callback: CallbackContext):
+    def rate_info(self, update: Update, context: CallbackContext):
         update.effective_message.reply_text("Rate info")
 
-    def exchange(self, update: Update, callback: CallbackContext):
-        update.effective_message.reply_text("Exchange")
+    def exchange(self, update: Update, context: CallbackContext):
+        chat_id = update.effective_chat.id
+
+        # Do some cool stuff here...
+
+        m = update.effective_message.reply_text(
+            "OK.", reply_markup=ReplyKeyboardRemove())
+        context.bot.delete_message(chat_id, message_id=m.message_id)
+
+        update.effective_message.reply_photo(
+            photo=open(custom('paths')['choose_exchange_type'], 'rb'),
+            caption=text('choose_exchange_type'), parse_mode='HTML', reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(button('uzs_to_usd'),
+                                      callback_data='uzs-to-usd')],
+                [InlineKeyboardButton(button('usd_to_uzs'),
+                                      callback_data='usd-to-uzs')],
+                [InlineKeyboardButton(button('back'),
+                                      callback_data='back')]
+            ]))
+
+        return 'EXCHANGE_CALCULATOR_TYPE'
+
+    def back_to_display(self, update: Update, context: CallbackContext):
+
+        if update.callback_query:
+            query = update.callback_query
+            query.answer()
+            query.delete_message()
+
+        return self.display(update, context)
