77f780d6c456b0201ad37ad3686e21e3a875dd78
Nuriddin Islamov <nuriddin.k2003@gmail.com>
2022-06-17 22:54:05 +0100

    Implement complete refactoring of calculator

--- src/components/calculator.py
+++ src/components/calculator.py
@@ -1,24 +1,39 @@
 from telegram import InlineKeyboardMarkup, Update, ReplyKeyboardMarkup
 from telegram.ext import CallbackContext
-from utils.text import button
+from utils.text import button, text
+from utils.misc import format_currency
 from src.components import commands
 from typing import List
+import requests
+from bs4 import BeautifulSoup
+import html
+import re
 
 
 class Calculator():
 
     @staticmethod
     def __get_currency_rates():
-        UZS_TO_USD = 1130090
-        USD_TO_UZS = 1145001
+        # URL to fetch to get the data from
+        URL = 'https://bank.uz/currency/dollar-ssha'
+
+        # Make a GET request
+        res = requests.get(URL)
+
+        html_doc = html.unescape(res.text)
+        soup = BeautifulSoup(html_doc, 'html.parser')
+
+        # Not a typo. 'curs' shorthand for currencies
+        curs = soup.find_all('div', class_='col-4')
+        print(type(curs))
+
+        UZS_TO_USD = 11300.90
+        USD_TO_UZS = 11450.01
+
         return {
             'uzs-to-usd': UZS_TO_USD,
             'usd-to-uzs': USD_TO_UZS
         }
-
-    @staticmethod
-    def __format_currency(n: int):
-        return '{:,.2f}'.format(n)
 
     @staticmethod
     def __create_default_amounts_markup(
@@ -31,9 +46,10 @@ class Calculator():
         This method builds a markup for currency default amounts
         """
         amounts = [
-            f'${self.__format_currency(i)}' if is_dollar else f'{self.__format_currency(i)} UZS' for i in amounts]
+            f'${format_currency(amount)}' if is_dollar else f'{format_currency(amount)} UZS' for amount in amounts]
 
         markup = [amounts[i:i + 2] for i in range(0, len(amounts), 2)]
+
         if header_buttons:
             markup.insert(0, header_buttons if isinstance(
                 header_buttons, list) else [header_buttons])
@@ -49,7 +65,7 @@ class Calculator():
         query.answer()
         data = query.data
         query.delete_message()
-        context.chat_data.update({
+        context.user_data.update({
             "user_id": chat_id,
             "operation": data
         })
@@ -58,43 +74,48 @@ class Calculator():
         # Options for default values are provided.
 
         if data == 'uzs-to-usd':
-            message = "🇺🇿 > 🇺🇸"
+            message = "🇺🇿 ➡️ 🇺🇸"
             defaults_markup = self.__create_default_amounts_markup(
                 self,
                 [1000, 5000, 10000, 100000, 250000], False,
                 footer_buttons=button('home_page')
             )
         elif data == 'usd-to-uzs':
-            message = "🇺🇸 > 🇺🇿"
+            message = "🇺🇸 ➡️ 🇺🇿"
             defaults_markup = self.__create_default_amounts_markup(
                 self,
-                [1, 5, 10, 200, 100], True,
+                [1, 5, 10, 200, 100000], True,
                 footer_buttons=button('home_page')
             )
         else:
             commands.Command().invalid_type(update, context)
 
-        context.bot.send_message(chat_id, f"{message}\nOK! Now choose how much you would like to exchange or type in your <b>custom amount</b>",
+        context.bot.send_message(chat_id, message + '\n\n' + text('choose_or_input_amount'),
                                  reply_markup=defaults_markup,
                                  parse_mode='HTML')
         return 'EXCHANGE_AMOUNT'
 
     def calculate(self, update: Update, context: CallbackContext):
         chat_id = update.effective_chat.id
-        message = int(update.effective_message.text)
-        rates = self.__get_currency_rates()
-        operation = context.chat_data['operation']
+        message = update.effective_message.text
 
+        rates = self.__get_currency_rates()
+        operation = context.user_data['operation']
+
+        # Convert the number format for further processing
+        amount = float(re.findall(
+            r'[\d\.\,\d]+', message)[0].replace(',', ''))
+
+        # TODO: Using a symbol to represent the currency
         if operation == 'uzs-to-usd':
-            out = message / (rates[operation] / 100)
+            out = amount / rates[operation]
+            symbol = "$"
         elif operation == 'usd-to-uzs':
-            out = (rates[operation] / 100) * message
+            out = rates[operation] * amount
+            symbol = " UZS"
         else:
             commands.Command().invalid_type(update, context)
 
-        send = ""
+        out_text = text('calculator_out').format("{:,.2f}".format(out))
 
-        context.bot.send_message(
-            chat_id, f"Here it is:\n{self.__format_currency(out)}")
-
-        # return "CALCULATE_OUT"
+        context.bot.send_message(chat_id, out_text, parse_mode='HTML')
